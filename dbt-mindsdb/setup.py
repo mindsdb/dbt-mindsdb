7dcb6346332faeca716d6d0230f058a0eaeb1ac0
ZoranPandovski <zoran.pandovski@gmail.com>
2022-03-02 14:53:16 +0100

    Update setup.py

--- dbt-mindsdb/setup.py
+++ dbt-mindsdb/setup.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python
 from setuptools import find_namespace_packages, setup
 
-package_name = "dbt-dbt-mindsdb"
+package_name = "dbt-mindsdb"
 # make sure this always matches dbt/adapters/dbt-mindsdb/__version__.py
 package_version = "1.0.1"
-description = """The dbt-mindsdb adapter plugin for dbt"""
+description = """The dbt adapter plugin for connecting to MindsDB"""
 
 setup(
     name=package_name,
     version=package_version,
     description=description,
     long_description=description,
-    author=<INSERT AUTHOR HERE>,
-    author_email=<INSERT EMAIL HERE>,
-    url=<INSERT URL HERE>,
+    author='MindsDB Inc',
+    author_email='admin@mindsdb.com',
+    url='https://github.com/mindsdb/dbt-mindsdb',
     packages=find_namespace_packages(include=['dbt', 'dbt.*']),
     include_package_data=True,
     install_requires=[
-        "dbt-core==1.0.1",
-        <INSERT DEPENDENCIES HERE>
+        "dbt-core==1.0.1"
     ]
 )
