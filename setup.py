5f7ee407696c3919b9bc022b0c47e966827ee177
ZoranPandovski <zoran.pandovski@gmail.com>
2022-05-30 18:44:00 +0200

    Publish to pypi

--- setup.py
+++ setup.py
@@ -3,14 +3,17 @@ from setuptools import find_namespace_packages, setup
 
 package_name = "dbt-mindsdb"
 # make sure this always matches dbt/adapters/mindsdb/__version__.py
-package_version = "1.0.1"
+package_version = "1.0.2"
 description = """The dbt adapter plugin for connecting to MindsDB"""
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
 
 setup(
     name=package_name,
     version=package_version,
     description=description,
-    long_description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='MindsDB Inc',
     author_email='admin@mindsdb.com',
     url='https://github.com/mindsdb/dbt-mindsdb',
