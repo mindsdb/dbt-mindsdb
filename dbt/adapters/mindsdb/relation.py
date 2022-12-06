827e0ba12848dad0e733ead9b48798b81773d5e2
NGUYEN HUU BACH <bachng@gmail.com>
2022-12-06 12:57:36 +0000

    add a null create_database sentence, default quote policy and a WA for mindsdb/#4152

---
+++ dbt/adapters/mindsdb/relation.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+
+from dbt.adapters.base.relation import BaseRelation, Policy
+from dbt.contracts.relation import ComponentName
+
+
+@dataclass
+class MindsdbQuotePolicy(Policy):
+    database: bool = False
+    schema: bool = False
+    identifier: bool = False
+
+
+@dataclass(frozen=True, eq=False, repr=False)
+class MindsdbRelation(BaseRelation):
+    quote_policy: MindsdbQuotePolicy = MindsdbQuotePolicy()
+
+    # Overridden as Mindsdb converts relation identifiers to lowercase
+    def _is_exactish_match(self, field: ComponentName, value: str) -> bool:
+       return self.path.get_lowered_part(field) == value.lower()
