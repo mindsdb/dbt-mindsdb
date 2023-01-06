633b19684dcb5139adcd7906cbd17160bd74c861
NGUYEN HUU BACH <bachng@gmail.com>
2023-01-06 10:09:37 +0000

    add prefix and fix drop table issue in database materialization and update table materialization

--- dbt/include/mindsdb/macros/materialization/database.sql
+++ dbt/include/mindsdb/macros/materialization/database.sql
@@ -1,22 +1,43 @@
 {% materialization database, adapter='mindsdb' %}
   {%- set database = model['alias'] -%}
   {%- set engine = config.get('engine') -%}
+  {%- set prefix = config.get('prefix') -%}
   {%- set parameters = config.get('parameters') -%}
 
+  {% if prefix is none %}
+    {%- set connector = database %}
+  {% else %}
+    {%- set connector =  prefix ~ "_" ~ database %}
+  {% endif %}
+  
+
   -- build model
-  {%- call statement('main') -%}
-    CREATE DATABASE {{ database }}
+
+  -- WA for https://github.com/mindsdb/mindsdb/issues/4152
+  {%- call statement('tables', fetch_result = True) -%}
+  SHOW DATABASES
   {%- endcall -%}
-  {%- call statement('main') -%}
-    DROP DATABASE IF EXISTS {{ database }}
-  {%- endcall -%}
+  {%- set tables = load_result('tables') -%}
+  {%- set tables_data = tables['data'] -%}
+  
+  {%- set found_table = False -%}
+  {% for item in tables_data %}
+    {% if item[0] == connector %}
+    {%- call statement('main') -%}
+      DROP DATABASE IF EXISTS {{ connector }}
+    {%- endcall -%}
+    {% endif %}
+  {% endfor %}
+
+  -- end WA
 
 
   {%- call statement('main') -%}
-    CREATE DATABASE {{ database }} WITH ENGINE='{{engine}}',
+    CREATE DATABASE {{ connector }} WITH ENGINE='{{engine}}',
     PARAMETERS={{parameters}}
   {%- endcall -%}
 
+  {{ log("Create mindsdb database(integration) \"" ~ connector ~ "\" with engine \"" ~ engine ~ "\"", True) }}
 
   -- Return the relations created in this materialization
   {{ return({'relations': []}) }}
